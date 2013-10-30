385721c697a93973b193105ecd364628d0a64d04
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2013-10-30 13:13:11 +0200

    sw: initialize irq and midi

--- sw/main.c
+++ sw/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <irq.h>
 #include <i2c.h>
 #include <codec.h>
 #include <delay.h>
 #include <sublime.h>
+#include <midi.h>
+
 static struct sublime sublime_synth;
 
 static void init(void)
 {
+	irq_init();
+
 #ifdef I2C_DRIVER
 	printf("Initializing i2c..");
 	i2c_init();
@@ -19,10 +24,15 @@ static void init(void)
 	printf("done\r\n");
 #endif
 
+	printf("Initializing MIDI..");
+	midi_init();
+	printf("done\r\n");
+
 	printf("Initializing sublime..");
 	sublime_init(&sublime_synth, 8);
 	printf("done\r\n");
 
+	irq_enable();
 }
 
 int main(void)
